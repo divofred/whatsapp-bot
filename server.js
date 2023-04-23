792f04afaf9540596efbeedf5eb09dcb9d24fdca
Fredrick Emmanuel <54718010+divofred@users.noreply.github.com>
2023-04-23 18:05:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
-const  {Configuration, OpenAIApi} = require('openai')
+const { Configuration, OpenAIApi } = require('openai');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!\n'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 const config = new Configuration({
-  apiKey: process.env.OPEN_AI_API
-})
+  apiKey: process.env.OPEN_AI_API,
+});
 const openai = new OpenAIApi(config);
 
-async function runCompletion(message){
+async function runCompletion(message) {
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: message,
-    max_tokens: 100
-  })
-  return completion.data.choices[0].text
+    max_tokens: 100,
+  });
+  return completion.data.choices[0].text;
 }
 const client = new Client({
   puppeteer: {
@@ -34,55 +43,59 @@ let eth;
 client.on('message', async message => {
   const mentions = await message.getMentions();
   const chat = await message.getChat();
-  if(mentions[0]?.isMe || !chat.isGroup){
+  if (mentions[0]?.isMe || !chat.isGroup) {
     const lowerMessage = message.body.toLowerCase();
-    let lowerMessage1 = lowerMessage.split(' ')
-    if (lowerMessage1[1] === "set") {
+    let lowerMessage1 = lowerMessage.split(' ');
+    if (lowerMessage1[1] === 'set') {
       //Use author for group chat
-      if(message.from !== '2348126887684@c.us' && message.author !== "2348126887684@c.us"){
-        return message.reply('Not Authorized to make this change')
+      if (
+        message.from !== '2348126887684@c.us' &&
+        message.author !== '2348126887684@c.us'
+      ) {
+        return message.reply('Not Authorized to make this change');
       }
       let setting = lowerMessage.split(' ');
       let sto = setting[1];
       let val = setting[3];
-  
+
       if (sto == 'eth') {
         eth = val;
       }
       if (sto === 'btc') {
         btc = val;
-        
       }
     } else {
       if (lowerMessage.includes('btc') || lowerMessage.includes('bitcoin')) {
         return message.reply(`Current bitcoin rate is ${btc}`);
       }
       if (lowerMessage.includes('hello')) {
-       return message.reply('Hi');
+        return message.reply('Hi');
       }
       if (lowerMessage.includes('eth') || lowerMessage.includes('ethereum')) {
         return message.reply(`Current ethereum rate is ${eth}`);
-      } if(message.body === '!everyone') {
-        if (chat.isGroup){
-          let text = "";
-        let mentions = [];
-    
-        for(let participant of chat.participants) {
-            const contact = await client.getContactById(participant.id._serialized);
-            
+      }
+      if (message.body === '!everyone') {
+        if (chat.isGroup) {
+          let text = '';
+          let mentions = [];
+
+          for (let participant of chat.participants) {
+            const contact = await client.getContactById(
+              participant.id._serialized
+            );
+
             mentions.push(contact);
             text += `@${participant.id.user} `;
+          }
+
+          return await chat.sendMessage(text, { mentions });
+        } else {
+          return message.reply('Must be used in a group');
         }
-    
-        return await chat.sendMessage(text, { mentions });
-  
-        }else{
-          return message.reply('Must be used in a group')
-        }
-    } else{
-      runCompletion(message.body).then(result=> message.reply(result) );
-    } 
+      } else {
+        runCompletion(message.body).then(result => message.reply(result));
+      }
     }
   }
- });
+});
 client.initialize();
