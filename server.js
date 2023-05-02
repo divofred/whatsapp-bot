540a69db3c40dba8eede82c4150eedb24ab8ce8a
Fredrick Emmanuel <54718010+divofred@users.noreply.github.com>
2023-05-02 01:03:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ async function runCompletion(message) {
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: message,
+    max_tokens: 200,
   });
   return completion.data.choices[0].text;
 }
